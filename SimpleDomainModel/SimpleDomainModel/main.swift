54fcd1c2d3ecac2ac267f1640052b275ccf93b46
Nathan Han <nh0713@uw.edu>
2018-10-15 14:36:04 -0700

    update

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -28,15 +28,65 @@ public struct Money {
   public var currency : String
   
   public func convert(_ to: String) -> Money {
+    switch self.currency {
+        case "USD":
+            switch to {
+                case "GBP":
+                    return Money(amount: self.amount / 2, currency: "GBP")
+                case "EUR":
+                    return Money(amount: self.amount / 2 + self.amount, currency: "EUR")
+                case "CAN":
+                    return Money(amount: self.amount / 4 + self.amount, currency: "CAN")
+                default:
+                    return Money(amount: self.amount, currency: self.currency)
+            }
+        case "GBP":
+            switch to {
+            case "USD":
+                return Money(amount: self.amount * 2, currency: "USD")
+            case "EUR":
+                return Money(amount: self.amount * 2 + self.amount, currency: "EUR")
+            case "CAN":
+                return Money(amount: self.amount * 2 + self.amount / 2, currency: "CAN")
+            default:
+                return Money(amount: self.amount, currency: self.currency)
+            }
+        case "EUR":
+            switch to {
+            case "USD":
+                return Money(amount: self.amount * 2 / 3, currency: "USD")
+            case "GBP":
+                return Money(amount: self.amount * 2 / 6, currency: "GBP")
+            case "CAN":
+                return Money(amount: self.amount * 2 / 3 + (self.amount * 2 / 12), currency: "CAN")
+            default:
+                return Money(amount: self.amount, currency: self.currency)
+            }
+        case "CAN":
+            switch to {
+                case "USD":
+                    return Money(amount: self.amount * 4 / 5, currency: "USD")
+                case "GBP":
+                    return Money(amount: self.amount * 4 / 10, currency: "GBP")
+                case "EUR":
+                    return Money(amount: self.amount * 6 / 5, currency: "EUR")
+                default:
+                    return Money(amount: self.amount, currency: self.currency)
+            }
+        default:
+            return Money(amount: self.amount, currency: "Unknown Currency")
+    }
   }
-  
+    
   public func add(_ to: Money) -> Money {
+    return Money(amount : self.convert(to.currency).amount + to.amount, currency: to.currency)
   }
   public func subtract(_ from: Money) -> Money {
+    return Money(amount : self.convert(from.currency).amount - from.amount, currency: from.currency)
   }
 }
 
-////////////////////////////////////
+//////////////////////////////////
 // Job
 //
 open class Job {
@@ -46,21 +96,53 @@ open class Job {
   public enum JobType {
     case Hourly(Double)
     case Salary(Int)
+    
+    func getHourly() -> Double {
+        switch self {
+            case .Hourly(let hourly):
+                return hourly
+            default:
+                return 0.0
+        }
+    }
+    
+    func getSalary() -> Int{
+        switch self {
+            case .Salary(let salary):
+                return salary
+            default:
+                return 0
+        }
+    }
   }
-  
+
   public init(title : String, type : JobType) {
+    self.title = title
+    self.type = type
   }
-  
+
   open func calculateIncome(_ hours: Int) -> Int {
+    switch self.type {
+    case .Hourly(let hourly):
+        return Int(hourly * Double(hours))
+    case .Salary(let sal):
+        return sal
+    }
   }
-  
+
   open func raise(_ amt : Double) {
+    switch self.type {
+    case .Hourly(let hourly):
+        self.type = .Hourly(amt + hourly)
+    case .Salary(let sal):
+        self.type = .Salary(Int(amt) + sal)
+    }
   }
 }
 
-////////////////////////////////////
+//////////////////////////////////
 // Person
-//
+
 open class Person {
   open var firstName : String = ""
   open var lastName : String = ""
@@ -68,25 +150,40 @@ open class Person {
 
   fileprivate var _job : Job? = nil
   open var job : Job? {
-    get { }
+    get {
+        return self._job
+    }
     set(value) {
+        if self.age < 16 {
+            self._job = nil
+        } else {
+            self._job = value
+        }
     }
   }
-  
+
   fileprivate var _spouse : Person? = nil
   open var spouse : Person? {
-    get { }
+    get {
+        return self._spouse
+    }
     set(value) {
+        if self.age < 18 {
+            self._spouse = nil
+        } else {
+            self._spouse = value
+        }
     }
   }
-  
+
   public init(firstName : String, lastName: String, age : Int) {
     self.firstName = firstName
     self.lastName = lastName
     self.age = age
   }
-  
+
   open func toString() -> String {
+    return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(self._job) spouse:\(self._spouse)]"
   }
 }
 
@@ -95,13 +192,13 @@ open class Person {
 //
 open class Family {
   fileprivate var members : [Person] = []
-  
+
   public init(spouse1: Person, spouse2: Person) {
   }
-  
+
   open func haveChild(_ child: Person) -> Bool {
   }
-  
+
   open func householdIncome() -> Int {
   }
 }
